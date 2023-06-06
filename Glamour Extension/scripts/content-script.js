c01eac53eba36fd437c1f5db82b608a02269aece
rhubarbhill <90863727+rhubarbhill@users.noreply.github.com>
2023-06-06 17:40:19 -0500

    Making a Table on the Same Page
    Decided to put the table not in the popup (which is not too convenient) but on the page itself. It was during this time when I finally found out how to easily find the JS path using Inspect Element, and that helped a lot.

--- Glamour Extension/scripts/content-script.js
+++ Glamour Extension/scripts/content-script.js
@@ -8,6 +8,8 @@ var album_info = document.getElementsByClassName("album_info_outer")
 var artist_field = album_info[0].getElementsByClassName("artist")
 artist_name = artist_field[0].innerText
 
+var album_info_end = document.querySelector("#column_container_right > div.section_main_info.section_outer > div > table.album_info_outer")
+
 var song
 var blank = ' '
 for (let i = 0; i < all_tracks.length; i++) {
@@ -27,11 +29,31 @@ for (let i = 0; i < all_tracks.length; i++) {
     }
 }
 
+let table = "<table><tr><th>Song</th><th>Artist</th></tr><tr><td id='song-holder'>SONG</td><td id='artist-holder'>ARTIST</td></tr></table>"
+//TODO: ^ Figure out how to make this shorter while still containing the same thing
+album_info_end.insertAdjacentHTML("afterend", table)
+
+var curr_song = "..."
+var curr_artist = "..."
+var content = ''
+var holder = ''
+
 function pencil(song_title) {
     console.log(song_title)
     chrome.runtime.sendMessage({
         song: song_title,
         artist: artist_name
     })
+
+    write_cell(curr_song, song_title, "song-holder")
+    write_cell(curr_artist, artist_name, "artist-holder")
+
     // TODO: Make it so the pencil works even when the popup is closed
+}
+
+function write_cell (curr_, message_, id) {
+    curr_ = message_
+    content = curr_
+    holder = document.getElementById(id)
+    holder.innerHTML = content
 }
