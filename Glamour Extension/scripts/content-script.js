0ab3d903b290b65568ff647fb0023503354c4dcb
rhubarbhill <90863727+rhubarbhill@users.noreply.github.com>
2023-06-06 01:26:11 -0500

    Test for the Pencil Function
    Made the pencil function, which is not carrying out its eventual intended function yet but is merely printing the song title to the console just to see how I can do that. This was harder than expected

--- Glamour Extension/scripts/content-script.js
+++ Glamour Extension/scripts/content-script.js
@@ -8,11 +8,20 @@ var song
 var blank = ' '
 for (let i = 0; i < all_tracks.length; i++) {
     if (all_tracks[i].style.textAlign != 'right') {
-    song = all_tracks[i].getElementsByClassName("rendered_text")
-    btn = document.createElement("button")
-    btn.innerText = '📋'
-    song[0].insertAdjacentElement("afterend", btn)
-    song[0].insertAdjacentText("afterend", blank)
-    console.log(song[0].innerText)
+    this["song"+i] = all_tracks[i].getElementsByClassName("rendered_text")
+    this["btn_pencil"+i] = document.createElement("button")
+    this["btn_pencil"+i].innerText = '✏️'
+    const song_text = this["song"+i][0].innerText
+    this["btn_pencil"+i].onclick = function() {pencil(song_text)}
+    this["btn_clip"+i] = document.createElement("button")
+    this["btn_clip"+i].innerText = '📋'
+    this["song"+i][0].insertAdjacentElement("afterend", this["btn_clip"+i])
+    this["song"+i][0].insertAdjacentElement("afterend", this["btn_pencil"+i])
+    this["song"+i][0].insertAdjacentText("afterend", blank)
+    console.log(song_text)
     }
+}
+
+function pencil(song_title) {
+    console.log(song_title)
 }
