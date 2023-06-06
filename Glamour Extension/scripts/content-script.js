0c01e63c78277526c868cce761148e05517fe7c1
rhubarbhill <90863727+rhubarbhill@users.noreply.github.com>
2023-06-05 23:29:52 -0500

    Getting Clipboard Buttons Next to Track Titles
    It took a lot of workshopping and tinkering and Googling, but I managed to get it so clipboard buttons automatically appeared next to track titles. There's not too many lines of code here, but a lot of scrapped code and trial + error went into this

--- Glamour Extension/scripts/content-script.js
+++ Glamour Extension/scripts/content-script.js
@@ -3,14 +3,16 @@ console.log('scripts/content-script.js')
 //var tracklist = document.getElementsByClassName("tracks tracklisting")
 var tracklist = document.getElementById("tracks")
 var all_tracks = tracklist.getElementsByClassName("track")
-//var babylon = all_tracks[0].getElementsByClassName("rendered_text")
-//babylon[0].innerText
-////Same as all_tracks[0].getElementsByClassName("rendered_text")[0].innerText
 
 var song
+var blank = ' '
 for (let i = 0; i < all_tracks.length; i++) {
     if (all_tracks[i].style.textAlign != 'right') {
     song = all_tracks[i].getElementsByClassName("rendered_text")
+    btn = document.createElement("button")
+    btn.innerText = '📋'
+    song[0].insertAdjacentElement("afterend", btn)
+    song[0].insertAdjacentText("afterend", blank)
     console.log(song[0].innerText)
     }
 }
